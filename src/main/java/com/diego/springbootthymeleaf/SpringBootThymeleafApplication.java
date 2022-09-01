154ba538d18cc9bb8a514c5c4c49071943e27f13
Diego Oliveira <diegodsoliveira@gmail.com>
2022-09-01 14:36:09 -0300

    melhorias design

--- src/main/java/com/diego/springbootthymeleaf/SpringBootThymeleafApplication.java
+++ src/main/java/com/diego/springbootthymeleaf/SpringBootThymeleafApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.Ordered;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
